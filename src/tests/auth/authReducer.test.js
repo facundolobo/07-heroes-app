5468c38840389bbdc7a37ef81b297df634e3dbd2
Facundo Lobo <facundolobo2@gmail.com>
2021-03-01 01:22:07 -0300

    4. Pruebas en el authReducer

--- src/tests/auth/authReducer.test.js
+++ src/tests/auth/authReducer.test.js
@@ -1,31 +1,53 @@
-import { shallow } from "enzyme/build"
-import { authReducer } from "../../auth/authReducer";
 
-describe('Pruebas en <authReducer/>', () => {
+import { authReducer } from "../../auth/authReducer";
+import { types } from "../../types/types";
+
+describe('Pruebas en authReducer', () => {
 
 // const state={
 //     name: '',
 //     logged:false
 // }
 
-    
-
+   
     test('Debe de retornar el estado por defecto ', () => {
         const state= authReducer( {logged:false} , {} )
 
-        console.log(state);
+        
         expect(state).toEqual({logged:false})
     })
 
     test('debe de autentificar y colocar el name del usuario ', () => {
-        
+        const action={
+            type : types.login,
+            payload:{
+                name: 'fabrizio'
+               }
+        }
+        const state= authReducer( {logged:false} , action )
+       
+
+        expect(state).toEqual({
+            logged: true,
+            name: 'fabrizio'
+        })
+
     })
     test('debe borrar el name y logged en false', () => {
         
-    })
-    
+        
+        const action={
+            type : types.logout,
+            }
 
-    
-    
-    
+        const state= authReducer( {logged:true , name:'Fabrizio'} , action )
+   
+        
+        //verificamos que se subiera 
+        expect(state).toEqual({
+            logged: false,
+            
+        })
+        
+    })
 })
